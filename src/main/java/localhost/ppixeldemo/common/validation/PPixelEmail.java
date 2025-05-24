8aaf438de77bf44a9938882eaecfefa9dce74146
Dzmitry Prakapenka <dima.prokopenko@gmail.com>
2025-05-25 00:47:13 +0300

    update custom validation annotations

--- src/main/java/localhost/ppixeldemo/common/validation/PPixelEmail.java
+++ src/main/java/localhost/ppixeldemo/common/validation/PPixelEmail.java
@@ -4,8 +4,6 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -19,11 +17,4 @@ import java.lang.annotation.Target;
 @Target({METHOD, FIELD, ANNOTATION_TYPE, PARAMETER})
 @Retention(RUNTIME)
 @Documented
-@Constraint(validatedBy = {})
-public @interface PPixelEmail {
-  String message() default "Email should be valid and contains at most 200 symbols";
-
-  Class<?>[] groups() default {};
-
-  Class<? extends Payload>[] payload() default {};
-}
+public @interface PPixelEmail {}
